0fd3c53cb616b9c06397e499cad8058acce8c38e
Gabriel Santos <dreenusg2403@gmail.com>
2021-01-06 17:40:34 -0300

    senhatemp

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function confirmar() {
+    var n = window.document.getElementById('senha')
+    var unlock = window.document.getElementById('unlocked')
+    var s = Number(n.value)
+    if(s == 209){
+        unlock.innerHTML = '--> <a href="./secreto.html" target="_blank"><input type="button" value="Clique Aqui!" onclick="aqui()"></a> <--'
+    } else {
+        unlock.innerHTML = 'Senha Errada!'
+    }
+}
+
+function dica() {
+    var w = window.document.getElementById('wey')
+    w.innerHTML = 'Tente fazer algo com os números que achar...'
+}
