a8a3a38705e5a567cc5bd11f995a3ab5f13d8e72
Gabriel Santos <dreenusg2403@gmail.com>
2021-02-04 22:45:43 -0300

    geraledark

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-function confirmar() {
-    var n = window.document.getElementById('senha')
-    var unlock = window.document.getElementById('unlocked')
-    var s = Number(n.value)
-    if(s == 209){
-        unlock.innerHTML = '--> <a href="./secreto.html" target="_blank"><input type="button" value="Clique Aqui!" onclick="aqui()"></a> <--'
-    } else {
-        unlock.innerHTML = 'Senha Errada!'
-    }
+function dark() {
+    var botao = window.document.getElementById('darkb')
+    var td = window.document.getElementById('tudo')
+    var fundo = window.document.getElementById('des')
+    var corpo = window.document.body
+    botao.src = "./img/darkbon.png"
+    corpo.style.background = "rgb(53, 54, 58)"
+    fundo.style.background = "#1c1c1c"
+    td.style.color = "#fff"
 }
